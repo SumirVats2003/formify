2f1ccdc24e95af1d0baa3ffcdae0605ae83dc709
Sumir Vats <sumirvats@gmail.com>
2025-08-01 02:52:35 +0000

    change env load error message

--- backend/main.go
+++ backend/main.go
@@ -13,7 +13,7 @@ import (
 
 func main() {
 	if err := godotenv.Load(); err != nil {
-		log.Println("Warning: .env file not found, using environment variables")
+		log.Println("Warning: .env file not found, using empty variables")
 	}
 
 	port := ":8080"
