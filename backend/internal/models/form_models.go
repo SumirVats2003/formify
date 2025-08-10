29e208f1c405718ef1c808b76fddf6e9a85ca1fd
Sumir Vats <sumirvats@gmail.com>
2025-08-10 03:36:14 +0000

    added form models

---
+++ backend/internal/models/form_models.go
@@ -0,0 +1,33 @@
+package models
+
+type Form struct {
+	Id                string   `json:"id"`
+	Title             string   `json:"title"`
+	CreatorId         string   `json:"creatorId"`
+	Questions         []string `json:"questions"`
+	AttachedSheet     string   `json:"sheetUrl"`
+	ValidityTimestamp int64    `json:"validityTimestamp"`
+}
+
+type FormRequest struct {
+	Title             string            `json:"title"`
+	CreatorId         string            `json:"creatorId"`
+	Questions         []QuestionRequest `json:"questions"`
+	ValidityTimestamp int64             `json:"validityTimestamp"`
+}
+
+type Question struct {
+	Id            string   `json:"id"`
+	FormId        string   `json:"formId"`
+	QuestionTitle string   `json:"title"`
+	AnswerType    string   `json:"answerType"`
+	Required      bool     `json:"required"`
+	Options       []string `json:"options"`
+}
+
+type QuestionRequest struct {
+	QuestionTitle string   `json:"title"`
+	AnswerType    string   `json:"answerType"`
+	Required      bool     `json:"required"`
+	Options       []string `json:"options"`
+}
